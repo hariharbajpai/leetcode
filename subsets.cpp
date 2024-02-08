85f0f40593b916d95622bf4a622705b685485a2f
hariharbajpai <harihar701764@gmail.com>
2024-02-08 09:28:10 +0530

    s

---
+++ subsets.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class Solution {
+private:
+    void solve(vector<int>& nums, vector<int> output, int index, vector<vector<int>>& ans) {
+        // Base case
+        if (index >= nums.size()) {
+            ans.push_back(output);
+            return;
+        }
+        // Exclude
+        solve(nums, output, index + 1, ans);
+        // Include
+        output.push_back(nums[index]);
+        solve(nums, output, index + 1, ans);
+    }
+
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> output;
+        int index = 0;
+        solve(nums, output, index, ans);
+        return ans;
+    }
+};
+int main(){
+
+    return 0;
+}
