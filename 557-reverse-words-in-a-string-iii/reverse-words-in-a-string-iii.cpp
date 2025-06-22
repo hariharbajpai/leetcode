4a857681e64cb3d92020a9aaef8039a167eb3c3d
Harihar Bajpai <143341040+hariharbajpai@users.noreply.github.com>
2025-06-22 22:49:17 +0530

    Time: 5 ms (35.84%) | Memory: 13.5 MB (48.81%) - LeetSync

---
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stack<char>st;
+        string result;
+
+        for(char c : s){
+            if(c != ' '){
+                st.push(c);
+            } else {
+                while (!st.empty()) {
+                    result += st.top();
+                    st.pop();
+                }
+                result += ' ';
+            }
+        }
+        while (!st.empty()) {
+            result += st.top();
+            st.pop();
+        }
+        
+        return result;
+    }
+};
