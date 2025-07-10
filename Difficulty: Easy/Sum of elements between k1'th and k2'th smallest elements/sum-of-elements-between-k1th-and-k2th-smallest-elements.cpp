53e39721e3576845cf859a5d51cde908f925fd39
Harihar Bajpai <143341040+hariharbajpai@users.noreply.github.com>
2025-07-10 13:54:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum of elements between k1'th and k2'th smallest elements/sum-of-elements-between-k1th-and-k2th-smallest-elements.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long sumBetweenTwoKth(long long A[], long long N, long long K1, long long K2) {
+        
+        sort(A, A + N);
+        
+        long long sum = 0;
+        
+        
+        for (long long i = K1; i < K2 - 1; ++i) {
+            sum += A[i];
+        }
+        
+        return sum;
+    }
+};
