b7c6730f50695b9591b2e39cfebadff2e2919b72
Harihar Bajpai <143341040+hariharbajpai@users.noreply.github.com>
2025-07-10 06:28:28 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (13.10%) - LeetSync

--- 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.cpp
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 class Solution {
 public:
-    long long pickGifts(vector<int>& gifts, int k) {  // Change return type to long long
-        // Create a max-heap from the gifts
+    long long pickGifts(vector<int>& gifts, int k) {   
+         
         priority_queue<int> pq(gifts.begin(), gifts.end());
 
-        // Perform k operations
+        
         for (int i = 0; i < k; i++) {
             if (pq.empty()) break;
             
@@ -19,7 +19,7 @@ public:
             pq.push(newGifts);
         }
 
-        // Calculate the total remaining gifts (use long long to prevent overflow)
+       
         long long total = 0;
         while (!pq.empty()) {
             total += pq.top();
