c91c66df1b4d246f06070518d9d0fd93bc11d8a3
Harihar Bajpai <143341040+hariharbajpai@users.noreply.github.com>
2025-07-12 13:47:04 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (87.64%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        //fibonacci
         if(n<=1) return 1;
-        int prev1=1 , prev2=1 , curr;
-        for(int  i =2 ;i<=n;i++){
-            curr = prev1 + prev2;
-            prev2 = prev1;
+        int prev1 = 1, prev2 = 1 ,curr;
+        for(int i =2;i<=n;i++){
+            curr = prev1+prev2;
+            prev2=prev1;
             prev1 = curr;
         }
         return curr;
-    }
+       }
 };
